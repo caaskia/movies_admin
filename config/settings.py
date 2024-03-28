f43e77e77b295959c63b2c3e1904dbff6228f960
caas <caaskia@gmail.com>
2024-03-28 10:15:48 +0300

    Review correct

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
