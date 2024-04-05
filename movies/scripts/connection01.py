3774cdb14af805ee99014457a943a5748321513d
caas <caaskia@gmail.com>
2024-04-05 17:09:36 +0300

    python manage.py runscript connection01

--- movies/scripts/connection01.py
+++ movies/scripts/connection01.py
@@ -1,10 +1,14 @@
-from django.db import connection
+# python manage.py runscript connection01
 
+from django.db import connection
 from movies.models import Genre
 
-if __name__ == '__main__':
+def run():
     genres = Genre.objects.all()
-    connection.queries  # []
+    qry = connection.queries  # []
+    print(qry)
     print(genres)
     print(connection.queries)  # [{'sql': 'SELECT "content"."genre"."created_at", "content"."genre"."updated_at", "content"."genre"."id", "content"."genre"."name", "content"."genre"."description" FROM "content"."genre" LIMIT 21', 'time': '0.005'}]
     print(len(connection.queries))  # 1
+
+
